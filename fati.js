390589c55c4fb73535151aead67b406ed80a9538
GreenY3z <108115427+GreenY3z@users.noreply.github.com>
2023-06-11 18:11:12 +0100

    myfolio

---
+++ fati.js
@@ -0,0 +1,11 @@
+let aboutEl = document.getElementById("about-el")
+let contactEl = document.getElementById("contacts")
+let result = ""
+
+
+
+
+function hire() {
+    contactEl.textContent = result
+    console.log(result)
+}
